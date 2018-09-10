a74c1b38d38654e9921b6dea60127deefcc2dea4
user142822 <user142822@as106.local>
2018-09-10 16:32:42 -0700

    Initial Commit

---
+++ Tippster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippster
+//
+//  Created by user142822 on 9/10/18.
+//  Copyright © 2018 user142822. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
